071745d3cce020d2eedfaf9a120302c85a7be7aa
unknown <you@example.com>
2020-04-16 03:49:49 +0530

    first commit

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #DJANGO WHITENOISE CONFIGURATION
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,33 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+if os.getcwd() == '/app':
+
+    import dj_database_url
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+
+    DATABASES['default'].update(db_from_env)
+
+    #Honor the 'X-forwarded-Proto' header for request.is_secure().
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+
+    #Allow all host headers
+
+    ALLOWED_HOSTS = ['pavanasapp.herokuapp.com']
+
+    DEBUG = True
+
+
+
+    #Static asset configuration
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
